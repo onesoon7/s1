8073dade2964794d83e5d6a2c5f6fb1015bd9b90
罗雨 <fuzaogou@gmail.com>
2017-02-09 10:44:25 +0800

    结构体

--- struct.c
+++ struct.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>//strcpy需要用到该函数
 struct student {
 	char name[20];
 	char sex[8];
@@ -8,7 +9,10 @@ struct student {
 int main(){
 	
 	struct student stu={"lws","boy",17};//同时赋初值
-	printf("名字%s\n性别%s\n年龄%d\n",stu.name,stu.sex,stu.age);
+    struct student stus;	printf("名字%s\n性别%s\n年龄%d\n",stu.name,stu.sex,stu.age);
+stus.age=15;
+strcpy(stus.name,"lws");//字符不能直接用等号需要用到该函数
+printf("名字%s年龄%d\n",stus.name,stus.age);
 	return 0;
 	
 }
